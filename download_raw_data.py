525b0256be115f81aefa9e0470dc09c4a8be5560
Yatharth <yatharth.thakare211@vit.edu>
2024-04-22 20:04:53 +0530

    first

---
+++ download_raw_data.py
@@ -0,0 +1,78 @@
+import datetime
+import os
+import urllib.parse
+import urllib.request
+import json
+import shutil
+
+
+# KNMI operational test key from https://developer.dataplatform.knmi.nl/get-started#make-api-calls
+key = os.getenv('KNMI_API_KEY')
+def get_radar_data(api_key, tstamp,dirloc):
+    url = 'https://api.dataplatform.knmi.nl/open-data/v1/datasets/radar_reflectivity_composites/versions/2.0/files/RAD_NL25_PCP_NA_'+tstamp+'.h5/url'
+    headers = {'Authorization': api_key}
+
+    req = urllib.request.Request(url, headers=headers)
+    with urllib.request.urlopen(req) as response:
+        meta = response.read()
+
+    realurl = json.loads(meta)["temporaryDownloadUrl"]
+    req = urllib.request.Request(realurl)
+    fname=tstamp+".hf5"
+    print(fname)
+    exists = os.path.exists(dirloc)
+    if not exists:
+        # Create a new directory because it does not exist
+        os.makedirs(dirloc)
+
+    with urllib.request.urlopen(req) as response:
+        with open(dirloc+fname, 'wb') as location:
+            shutil.copyfileobj(response, location)
+
+def get_files_for_specific_timestamps(tstamps_list, dirloc):
+    files = []
+    for tstamp in tstamps_list:
+        files.append(dirloc+tstamp+".hf5")
+        get_radar_data(key, tstamp, dirloc)
+    return files
+
+def get_data_of_n_previous_hours(hours):
+    now = datetime.datetime.utcnow()
+    now = now - datetime.timedelta(hours=hours, minutes=5)
+    now -= datetime.timedelta(minutes=now.minute%5)
+
+    now.strftime("%Y%m%d%H%M")
+    files = []
+    start=now
+    for _ in range(0,hours*12): # data avaialble every 5 minutes, so 12 times per hour
+        tstamp = start.strftime("%Y%m%d%H%M")
+        files.append(tstamp+".hf5")
+        get_radar_data(key, tstamp, './data/raw_training/'+start.strftime("%Y-%m-%d-%H%M"))
+        start += datetime.timedelta(minutes=5)
+    return files
+
+def morning_filenames_for_day(year, month, day):
+    prefix = year+month+day
+    return     [prefix+'0805',prefix+'0810',prefix+'0815',prefix+'0820',prefix+'0825',prefix+'0830',
+                prefix+'0835',prefix+'0840',prefix+'0845',prefix+'0850',prefix+'0855',prefix+'0900',
+                prefix+'0905',prefix+'0910',prefix+'0915',prefix+'0920',prefix+'0925',prefix+'0930',
+                prefix+'0935',prefix+'0940',prefix+'0945',prefix+'0950',prefix+'0955',prefix+'1000',
+                prefix+'1005',prefix+'1010',prefix+'1015',prefix+'1020',prefix+'1025',prefix+'1030',
+                prefix+'1035',prefix+'1040',prefix+'1045',prefix+'1050',prefix+'1055',prefix+'1100']
+
+def afternoon_filenames_for_day(year, month, day):
+    prefix = year+month+day
+    return     [prefix+'2005',prefix+'2010',prefix+'2015',prefix+'2020',prefix+'2025',prefix+'2030',
+                prefix+'2035',prefix+'2040',prefix+'2045',prefix+'2050',prefix+'2055',prefix+'2100',
+                prefix+'2105',prefix+'2110',prefix+'2115',prefix+'2120',prefix+'2125',prefix+'2130',
+                prefix+'2135',prefix+'2140',prefix+'2145',prefix+'2150',prefix+'2155',prefix+'2200',
+                prefix+'2205',prefix+'2210',prefix+'2215',prefix+'2220',prefix+'2225',prefix+'2230',
+                prefix+'2235',prefix+'2240',prefix+'2245',prefix+'2250',prefix+'2255',prefix+'2300']
+
+# files = get_data_of_n_preevious_hours(3)
+YEAR = '2023'
+MONTH = '04'
+DAY = '01'
+tstamps_list = afternoon_filenames_for_day(YEAR, MONTH, DAY)
+DIR_LOC = './data/raw_validation/' + YEAR +'-'+MONTH + '-' + DAY + '-2005/'
+files = get_files_for_specific_timestamps(tstamps_list, DIR_LOC)
---
+++ lstm_model.py
@@ -0,0 +1,164 @@
+import os
+import glob
+import io
+import imageio
+import sklearn.model_selection as sk
+import matplotlib
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+import numpy as np
+import PIL
+import h5py
+import tensorflow as tf
+from tensorflow import keras
+from keras.models import Sequential
+from keras.layers import Conv3D
+from keras.layers import ConvLSTM2D
+from keras.layers import BatchNormalization
+from keras.layers import LeakyReLU
+from PIL import Image
+matplotlib.use("Agg")
+
+try:
+    # Disable all GPUS
+    tf.config.set_visible_devices([], 'GPU')
+    visible_devices = tf.config.get_visible_devices()
+    for device in visible_devices:
+        assert device.device_type != 'GPU'
+except:
+    # Invalid device or cannot modify virtual devices once initialized.
+    pass
+
+# larger possible dpi: 382x350
+def create_dataset_from_raw(directory_path, resize_to):
+    resize_width = resize_to[0]
+    resize_height = resize_to[1]
+    batch_names = [directory_path + name for name in os.listdir(directory_path) if os.path.isdir(os.path.join(directory_path, name))]
+    dataset = np.zeros(shape=(len(batch_names),36,resize_height,resize_width)) # (samples, filters, rows = height, cols = width)
+
+    for batch_idx,batch in enumerate(batch_names):
+        files = [x for x in os.listdir(batch) if x != '.DS_Store']
+        files.sort()
+        crn_batch = np.zeros(shape=(36, resize_height, resize_width))
+        for (idx,raster) in enumerate(files):
+            fn = batch + '/' + raster
+            img = h5py.File(fn)
+            original_image = np.array(img["image1"]["image_data"]).astype(float)
+            img = Image.fromarray(original_image)
+            # note that here it is (width, heigh) while in the tensor is in (rows = height, cols = width)
+            img = img.resize(size=(resize_width, resize_height))
+            original_image = np.array(img)
+            original_image = original_image / 255.0
+            crn_batch[idx] = original_image
+        dataset[batch_idx] = crn_batch
+        print("Importing batch:" + str(batch_idx+1))
+    return dataset
+
+def split_data_xy(data):
+    x = data[:, 0 : 18, :, :]
+    y = data[:, 18 : 36, :, :]
+    return x, y
+
+dataset = create_dataset_from_raw('./data/raw_training/', resize_to=(315,344))
+dataset = np.expand_dims(dataset, axis=-1)
+dataset_x, dataset_y = split_data_xy(dataset)
+X_train, X_val, y_train, y_val = sk.train_test_split(dataset_x,dataset_y,test_size=0.1, random_state = 42)
+
+def create_model():
+    model = Sequential()
+    model.add(ConvLSTM2D(filters=64, kernel_size=(7, 7),
+                    input_shape=(18,344,315,1),
+                    padding='same',activation=LeakyReLU(alpha=0.01), return_sequences=True))
+    model.add(BatchNormalization())
+    model.add(ConvLSTM2D(filters=64, kernel_size=(5, 5),
+                    padding='same',activation=LeakyReLU(alpha=0.01), return_sequences=True))
+    model.add(BatchNormalization())
+    model.add(ConvLSTM2D(filters=64, kernel_size=(3, 3),
+                    padding='same',activation=LeakyReLU(alpha=0.01), return_sequences=True))
+    model.add(BatchNormalization())
+    model.add(ConvLSTM2D(filters=64, kernel_size=(1, 1),
+                    padding='same',activation=LeakyReLU(alpha=0.01), return_sequences=True))
+    model.add(Conv3D(filters=1, kernel_size=(3, 3, 3),
+                activation='sigmoid',
+                padding='same', data_format='channels_last'))
+    return model
+
+model = create_model()
+
+model.compile(loss='binary_crossentropy', optimizer='adadelta')
+print(model.summary())
+
+EPOCHS = 25
+BATCH_SIZE = 1
+
+#Fit the model
+model.fit(
+    X_train,
+    y_train,
+    batch_size=BATCH_SIZE,
+    epochs=EPOCHS,
+    validation_data=(X_val, y_val),
+    verbose=1,
+)
+model.save('./model_saved')
+
+# pick a random index from validation dataset
+random_index = np.random.choice(range(len(X_val)), size=1)
+test_serie_X = X_val[random_index[0]]
+test_serie_Y = y_val[random_index[0]]
+
+first_frames = test_serie_X
+original_frames = test_serie_Y
+
+# predict the next 18 fames
+new_prediction = model.predict(np.expand_dims(first_frames, axis=0))
+new_prediction = np.squeeze(new_prediction, axis=0)
+
+fig, axes = plt.subplots(2, 18, figsize=(20, 4))
+
+# Plot the original frames.
+for idx, ax in enumerate(axes[0]):
+    ax.imshow(np.squeeze(original_frames[idx]), cmap="viridis")
+    ax.set_title(f"Frame {idx + 18}")
+    ax.axis("off")
+
+# Plot the predicted frames.
+for idx, ax in enumerate(axes[1]):
+    ax.imshow((new_prediction[idx]).reshape((344,315)), cmap="viridis")
+    ax.set_title(f"Frame {idx + 18}")
+    ax.axis("off")
+
+plt.show()
+
+def save_animation_original():
+    fig, ax = plt.subplots()
+    original_images = []
+    for f in original_frames:
+        ax.set_title("Ground Truth")
+        ax.axis("off")
+        crn_f = ax.imshow(np.squeeze(f),cmap='viridis', animated=False)
+        original_images.append([crn_f])
+    animation_originals = animation.ArtistAnimation(fig, original_images,
+                                                    interval=100, blit=False,
+                                                    repeat_delay=1000)
+    animation_originals.save('./ground_truth.gif',
+                            writer=animation.PillowWriter(), dpi=100)
+
+def save_animation_predicted():
+    fig, ax = plt.subplots()
+    predicted_images = []
+    for f in new_prediction:
+        ax.set_title("Ground Truth")
+        ax.axis("off")
+        crn_f = ax.imshow(np.squeeze(f),cmap='viridis', animated=False)
+        ax.set_title("Predicted frames")
+        predicted_images.append([crn_f])
+
+    animation_predicted = animation.ArtistAnimation(fig, predicted_images,
+                                                    interval=100, blit=False,
+                                                    repeat_delay=1000)
+    animation_predicted.save('./predicted.gif',
+                            writer=animation.PillowWriter(), dpi=100)
+
+save_animation_predicted()
+save_animation_original()
